e256ef7234bf738ccf4b9853e87d8844a528ea93
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-04-02 18:16:03 +0200

    02.04

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="cs">
 
-<?php $title = 'XML validátor' ?>
+<?php $title = 'PRI-XML srandy' ?>
 
 <head>
     <title><?= $title ?></title>
@@ -10,120 +10,70 @@
 <body>
     <h1><?= $title ?></h1>
 
-    <p>Nahrajte XML soubor, případně také XSD nebo DTD soubor.</p>
-    <hr>
-    <form enctype="multipart/form-data" method="POST">
-        <table>
-            <tr>
-                <td>XML soubor:</td>
-                <td>
-                    <!-- Allow users to choose from XML files on disk -->
-                    <select name="xml">
-                        <?php
-                        foreach (glob("xml/*.xml") as $file) {
-                            echo "<option value=\"$file\">$file</option>";
-                        }
-                        ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>XSD/DTD soubor:</td>
-                <td><input type="file" name="schema" data-max-file-size="2M"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" type="Odeslat"></td>
-            </tr>
-        </table>
-    </form>
-    <hr>
-
     <?php
-    function printErrors()
-    { ?>
-        <table>
-            <?php foreach (libxml_get_errors() as $error) { ?>
-                <tr>
-                    <td><?= $error->line ?></td>
-                    <td><?= $error->message ?></td>
-                </tr>
-            <?php } ?>
-        </table>
-    <?php }
-
-    function validate($xmlPath, $schemaPath = '')
-    {
-        $doc = new DOMDocument;
-
-        libxml_use_internal_errors(true);
-        $doc->loadXML(file_get_contents($xmlPath));
-        printErrors();
-        libxml_use_internal_errors(false);
-
-        // Do we have a root and a schema?
-        @$root = $doc->firstElementChild->tagName;
-        if ($root && $schemaPath) {
-            echo "<p>Validating using XSD. Root: <b>$root</b></p>";
-
-            $xsd = new DOMDocument;
-            $xsd->load($schemaPath);
-
-            libxml_use_internal_errors(true);
-            $isValid = $doc->schemaValidate($xsd);
-            printErrors();
-            libxml_use_internal_errors(false);
-
-            return $isValid;
+        // DB
+        $mysqli= new mysqli("localhost", "root", "", "pri_db");
+        $mysqli->query("Set names utf8");
+        if ($mysqli->connect_error) {
+            echo $mysqli->connect_error;
+            return;
         }
 
-        // Fallback to DTD validation
-        echo "<p>Validating using DTD. Root: <b>$root</b></p>";
+        /* SimpleXML */
+/*
+        // Získání všeho z xml
+        $xml = simplexml_load_file('xml/studenti.xml');
 
-        // ... (rest of the existing DTD validation logic)
+        function traverseSimpleXML($xml, $level=0) {
+            $space = function($level) {
+                return str_repeat(' ', $level * 2);
+            };
+        
+            $attributes = $xml->attributes();
+        
+            foreach ($attributes as $name => $value) {
+                echo $space($level) . "$name - " . (string)$value . "<br>";
+            }
+        
+            $children = $xml->children();
+            foreach ($children as $name => $value) {
+                if(0 < $value->count()) {
+                    echo $space($level) . "$name: <br>";
+                    traverseSimpleXML($value, $level+1);
+                } else {
+                    echo $space($level) . "$name = " . (string)$value . "<br>";
+                }
+            }
+        }
 
-        return $isValid;
-    }
+        traverseSimpleXML($xml);
+        
+        // Získání něčeho ze souboru    
+        $xml = simplexml_load_file('xml/studenti.xml');
+        echo (string)($xml->student[0]->note->Fname);
+*/
 
-    $xmlFile = @$_FILES['xml'];
-    $schemaFile = @$_FILES['schema'];
 
-    if (@$xmlTmpName = $xmlFile['tmp_name']) {
-        $schemaTmpName = $schemaFile['tmp_name'];
-        $isValid = validate($xmlTmpName, $schemaTmpName);
-        if ($isValid)
-            echo "The uploaded XML file is valid.";
-    }
+        // TVORBA z DB 
+        $xml = new SimpleXMLElement('<?xml-stylesheet type="text/xsl" href="xml/studenti.xsl"?><student/>');
+      
+        $sql= "SELECT * FROM student";
+		$result = $mysqli->query($sql);
+		while($row = $result->fetch_assoc()){
+            $studenti = $xml->addChild('student');
+            $studenti->addChild('jmeno',$row["Fname"]);
+            $studenti->addChild('primeni',$row["Lname"]);
+            $studenti->addChild('katedra',$row["katedra"]);
+            $studenti->addChild('rocnik',$row["studyYear"]);
+        }
+        
+        header('Content-Type: application/xml');
+        echo $xml->asXML();
+    
+    
+    
     ?>
 
-
-    <?php
-        function handleFormSubmission() {
-            $selectedXml = $_POST['xml'];
-            $schemaFile = @$_FILES['schema'];
-            echo $selectedXml;
-            
-            echo "<a href='".$selectedXml.".'>Studentů Catalog</a>";
-            // XML
-            $xml = new DOMDocument;
-            $xml->load($selectedXml);
-            // XSL
-            $xsl_special = substr($selectedXml, 0, -4);
-            $xsl = new DOMDocument;
-            $xsl->load($xsl_special.'.xsl');
-            // transformer
-            $xslt = new XSLTProcessor();
-            $xslt->importStylesheet($xsl);
-            $transformovany_xml = $xslt->transformToXml($xml);
-            // output
-            echo $transformovany_xml;
-
-        }
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            handleFormSubmission();
-        }
-    ?>
 
 </body>
 
